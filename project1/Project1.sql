d2b4db21e2cd2275002f077e52a300f08fa75660
Jacob Boullion <jacob.boullion@revature.net>
2021-02-04 18:22:33 -0600

    Final script

--- project1/Project1.sql
+++ project1/Project1.sql
@@ -1,194 +1,3 @@
---HQL Hive Query Language
-
-CREATE DATABASE student_db;
-
-SHOW DATABASES;
-
-USE student_db;
-
-CREATE TABLE STUDENT (
-	ssn STRING,
-	first_name STRING,
-	last_name STRING,
-	age INT,
-	state STRING,
-	house STRING)
-	ROW FORMAT DELIMITED
-	FIELDS TERMINATED BY ','
-	TBLPROPERTIES("skip.header.line.count"="1");
-	
-DESCRIBE STUDENT;
-
-LOAD DATA LOCAL INPATH '/home/jacob/student-house.csv' INTO TABLE STUDENT;
-
-SELECT * FROM STUDENT;
-
-DROP TABLE STUDENT;
-
---external table 
-
-CREATE EXTERNAL TABLE STUDENT (
-	ssn STRING,
-	first_name STRING,
-	last_name STRING,
-	age INT,
-	state STRING,
-	house STRING)
-	ROW FORMAT DELIMITED
-	FIELDS TERMINATED BY ','
-	LOCATION '/user/jacob/mydata'
-	TBLPROPERTIES("skip.header.line.count"="1");
-
-LOAD DATA INPATH '/user/jacob/student/student-house.csv' INTO TABLE STUDENT;
-
-SELECT * FROM STUDENT
-ORDER BY last_name DESC 
-LIMIT 50;
-
-SHOW TABLES;
-
-
-INSERT OVERWRITE DIRECTORY '/user/hive/output'
-ROW FORMAT DELIMITED 
-FIELDS TERMINATED BY '\t'
-SELECT house, COUNT(house) FROM STUDENT 
-GROUP BY house;
-
-
-
-SELECT first_name, last_name, state FROM STUDENT 
-WHERE UPPER(state) = 'VIRGINIA' OR UPPER(state) = 'CALIFORNIA';
-
-
-
-SELECT first_name, ssn FROM STUDENT 
-WHERE house = 'Hufflepuff' AND first_name LIKE 'C%';
-
-SELECT house, ROUND(AVG(AGE), 2) FROM student 
-GROUP BY house;
-
-
---managed tables 
-CREATE TABLE student_state 
-AS SELECT COUNT(*) AS Num_Students, state FROM student 
-GROUP BY state;
-
-SELECT * FROM student_state;
-
-
-SELECT * FROM student;
-CREATE TABLE STUDENT_AGE (
-	ssn STRING,
-	first_name STRING,
-	last_name STRING,
-	state STRING,
-	house STRING
-) PARTITIONED BY (age INT)
-ROW FORMAT DELIMITED 
-FIELDS TERMINATED BY ',';
-
-
-INSERT INTO TABLE STUDENT_AGE PARTITION(age=33)
-SELECT ssn, first_name, last_name, state, house FROM STUDENT WHERE age=33;
-
-
-INSERT INTO TABLE STUDENT_AGE PARTITION(age=37)
-SELECT ssn, first_name, last_name, state, house FROM STUDENT WHERE age=37;
-
-INSERT INTO TABLE STUDENT_AGE PARTITION(age=23)
-SELECT ssn, first_name, last_name, state, house FROM STUDENT WHERE age=23;
-
-
-SELECT * FROM STUDENT_AGE;
-
-SELECT * FROM STUDENT
-
-
-SET hive.exec.dynamic.partition=true;
-SET hive.exec.dynamic.partition.mode=nonstrict;
-
-
-INSERT INTO TABLE STUDENT_AGE PARTITION(age)
-SELECT ssn, first_name, last_name, state, house, age FROM STUDENT;
-
-
-ALTER TABLE STUDENT_AGE DROP PARTITION(age=32);
-
-
-CREATE TABLE student_house (
-	ssn STRING,
-	first_name STRING,
-	last_name STRING,
-	age INT,
-	state STRING
-)
-PARTITIONED BY (house STRING)
-ROW FORMAT DELIMITED 
-FIELDS TERMINATED BY ','
-TBLPROPERTIES("skip.header.line.count"="1");
-
-
---LOAD DATA INPATH '/user/jacob/mydata/student-house.csv' INTO TABLE student_house;
-
-INSERT INTO TABLE student_house PARTITION(house)
-SELECT ssn, first_name, last_name, age, state, house FROM student;
-
-SELECT * FROM student_house;
-
-
---bucketing 
-SET hive.enforce.bucketing = true;
-
-
-CREATE TABLE student_age_buckets (
-	ssn STRING,
-	first_name STRING,
-	last_name STRING,
-	age INT,
-	state STRING,
-	house STRING 
-)
-CLUSTERED BY (age) INTO 4 BUCKETS 
-ROW FORMAT DELIMITED 
-FIELDS TERMINATED BY ',';
-
-INSERT INTO student_age_buckets
-SELECT ssn, first_name, last_name, age, state, house FROM student;
-
-
-CREATE TABLE student_partition_buckets (
-	ssn STRING,
-	first_name STRING,
-	last_name STRING,
-	age INT,
-	state STRING
-)
-PARTITIONED BY (house STRING)
-CLUSTERED BY (age) INTO 4 BUCKETS 
-ROW FORMAT DELIMITED 
-FIELDS TERMINATED BY ',';
-
-INSERT INTO student_partition_buckets PARTITION(house)
-SELECT ssn, first_name, last_name, age, state, house FROM student;
-
-
-CREATE TABLE house_buckets (
-	ssn STRING,
-	first_name STRING,
-	last_name STRING,
-	age INT,
-	state STRING,
-	house STRING
-)
-CLUSTERED BY (house) INTO 3 BUCKETS 
-ROW FORMAT DELIMITED 
-FIELDS TERMINATED BY ',';
-
-
-INSERT INTO house_buckets
-SELECT ssn, first_name, last_name, age, state, house FROM student;
-
-
 ---------------------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------------------
 --Project 1----------------------------------------------------------------------------------------------
@@ -256,6 +65,12 @@ ORDER BY views DESC;
 
 --Question 3
 
+--starting at hotel California
+SELECT * FROM clickstream
+WHERE type='link' AND prev='Hotel_California'
+ORDER BY n DESC
+limit 5;
+
 --total page views 
 SELECT page_title, SUM(count_views)*31 AS views 
 FROM PAGEVIEW
@@ -273,22 +88,13 @@ ORDER BY views DESC;
 
 --The_Long_Run_(album), Bernie_Leadon, Glenn_Frey or Don_Henley, Eagles_(band) or Lois_Chiles
 
-SELECT SUM(n)
-FROM clickstream 
-WHERE curr = 'Hotel_California';
-
---starting at hotel California
 SELECT * FROM clickstream
 WHERE type='link' AND prev='Don_Henley'
 ORDER BY n DESC
 limit 5;
 
-SELECT * FROM clickstream
-WHERE prev='Hotel_California_(Eagles_album)'
-ORDER BY n DESC;
-
 -- All links from third fraction
-SELECT prev, SUM(n)
+SELECT prev, SUM(n) as clicks
 FROM clickstream 
 WHERE type = 'link' AND (prev = 'The_Long_Run_(album)' OR prev = 'Bernie_Leadon' 
 OR prev = 'Lois_Chiles' OR prev = 'Eagles_(band)' OR prev = 'Don_Henley' OR prev = 'Glenn_Frey')  
@@ -438,12 +244,18 @@ WHERE revision_is_identity_reverted = true
 GROUP BY page_title 
 ORDER BY count_title DESC;
 
+SELECT page_title, COUNT(page_title) AS count_title , SUM(revision_seconds_to_identity_revert)
+FROM revision
+WHERE revision_is_identity_reverted = true
+GROUP BY page_title 
+ORDER BY count_title DESC;
+
 --individual time to revert page
 SELECT page_title, event_timestamp, ROUND(revision_seconds_to_identity_revert/60, 2)
 FROM revision
 WHERE revision_is_identity_reverted = true AND (page_title = 'Teahouse' OR page_title = 'Sandbox' OR page_title = 'Elliot_Page')
 
-
+--views for each page
 SELECT page_title, SUM(count_views) AS views 
 FROM PAGEVIEW
 WHERE page_title = 'Teahouse' OR page_title = 'Sandbox' OR page_title = 'Elliot_Page'
@@ -458,8 +270,3 @@ WHERE page_title LIKE 'a%a' OR page_title LIKE 'A%a'
 GROUP BY page_title
 ORDER BY views DESC
 LIMIT 5;
-
-
-
-
-
